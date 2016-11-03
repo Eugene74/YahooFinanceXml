3490d2fc3176c5c950afb8be5718f41378d2d701
Eugene74 <ea74@ukr.net>
2016-11-03 18:04:15 +0200

    comment  delete

--- src/yahoofinancexml/Main.java
+++ src/yahoofinancexml/Main.java
@@ -11,7 +11,7 @@ import java.net.URL;
 public class Main {
     
     public static void main(String[] args) throws Exception {
-//путь к файлу
+
         String request = "http://query.yahooapis.com/v1/public/yql?format=xml&q=select%20*%20from%20" +
                 "yahoo.finance.xchange%20where%20pair%20in%20(\"USDEUR\",%20\"USDUAH\")&env=store://datatables.org/alltableswithkeys";
 
